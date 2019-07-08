b19925c3b638f8089c4baf2ec83f3b25501013e5
Brenda0904 <brenda_vd_berg@live.nl>
2019-07-08 09:26:55 +0200

    Create fathmmxf.sh

---
+++ fathmmxf.sh
@@ -0,0 +1,2 @@
+python2.7 /exports/sascstudent/brenda/fathmm/fathmm_xf_query.py /exports/sascstudent/brenda/envision/envision_filtered.vcf -v -o /exports/sascstudent/brenda/fathmm/fathmm_out.vcf
+python /exports/sascstudent/brenda/fathmm/fathmm_filter.py /exports/sascstudent/brenda/envision/envision_filtered.vcf /exports/sascstudent/brenda/fathmm/fathmm_out.vcf /exports/sascstudent/brenda/fathmm/fathmm_filtered.vcf
