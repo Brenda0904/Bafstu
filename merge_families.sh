09f623b09962544720818fbee960a610140a0d42
Brenda0904 <brenda_vd_berg@live.nl>
2019-07-08 09:51:21 +0200

    Delete merge_families.sh

--- merge_families.sh
+++
@@ -1,4 +0,0 @@
-for f in *.vcf ; do vcf-sort $f > sorted_$f ; done
-for g in sorted_* ; do bgzip -c $g > $g.gz ; done
-for h in *.vcf.gz ; do tabix -p vcf $h ; done
-vcf-merge *.vcf.gz > exomiser_multisample.vcf
