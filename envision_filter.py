53e60fae3738a8da4ebe0759b6d5f3b91d72b616
Brenda0904 <brenda_vd_berg@live.nl>
2019-07-08 09:53:11 +0200

    Update envision_filter.py

--- envision_filter.py
+++ envision_filter.py
@@ -1,47 +1,41 @@
-from vcf_parser import VCFParser
-import sys
-
 my_parser = VCFParser(infile=sys.argv[1], split_variants=True, check_info=True)
 infile = open(sys.argv[2])
 envision_file = infile.readlines()
 infile.close()
 headers = envision_file[0].strip('\n')
 headers = headers.split(',')
-print(headers)
 ref = headers.index('AA1')
 alt = headers.index('AA2')
 pos = headers.index('position')
 prot = headers.index('Uniprot')
 score = headers.index('Envision_predictions')
 variants = []
-for line in envision_file:
-    line = line.strip('\n')
-    line = line.split(',')
+for variant in my_parser:
     try:
-        for variant in my_parser:
-            key = next(iter(variant['vep_info']))
-            vep_info = variant['vep_info'][key][0]
-            print(vep_info)
-            aa_vep = vep_info['Amino_acids']
-            pos_vep = vep_info['Protein_position']
-            prot_vep = vep_info['SWISSPROT']
-            info = variant['info_dict']
-            envision_score = 0
-            if len(aa_vep) == 1 and line[pos] == pos_vep and line[prot] == prot_vep and line[alt] == i:
-                envision_score = line[score]
-                info['envision'] = envision_score
-                variants.append(variant)
-            elif len(aa_vep) > 1:
+        key = next(iter(variant['vep_info']))
+        vep_info = variant['vep_info'][key][0]
+        aa_vep = vep_info['Amino_acids']
+        pos_vep = vep_info['Protein_position']
+        prot_vep = vep_info['SWISSPROT']
+        info = variant['info_dict']
+        for x in range(2,len(envision_file)):
+            line = envision_file[x].strip('\n')
+            line = line.split(',')
+            if len(aa_vep) > 1:
                 amino_acids = aa_vep.split("/")
-                for i in amino_acids:
-                    if line[pos] == pos_vep and line[prot] == prot_vep and line[alt] == i and envision_score < line[score]:
-                        envision_score = line[score]
-                if line[pos] == pos_vep and line[prot] == prot_vep:
+                if line[pos] == pos_vep and line[prot] == prot_vep and line[ref] == amino_acids[0] and line[alt] == amino_acids[1]:
+                    envision_score = line[score]
                     info['envision'] = envision_score
                     variants.append(variant)
-                else:
-                    info['envision'] = ""
-                    variants.append(variant)
-            else:
-                info['envision'] = ''
-                variants.append(variant)
+                    break
+    except:
+        pass
+outfile = open(sys.argv[3], "w")
+for line in my_parser.metadata.print_header():
+    outfile.write(line + '\n')
+for variant in variants:
+    if len(variant['info_dict']['envision']) == 0:
+        outfile.write('\t'.join([variant[head] for head in my_parser.header])+ "\n")
+    elif float(variant['info_dict']['envision']) > 0.5:
+        outfile.write('\t'.join([variant[head] for head in my_parser.header])+ "\n")
+outfile.close()
