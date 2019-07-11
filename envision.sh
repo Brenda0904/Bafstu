3287d240e68e90befd373e83370517e22c93e07a
Brenda0904 <brenda_vd_berg@live.nl>
2019-07-11 10:14:30 +0200

    Update envision.sh

--- envision.sh
+++ envision.sh
@@ -1,3 +1,4 @@
+#envision.py is run to obtain the protein information from the variants in the exomiser output.
 python /exports/sascstudent/brenda/vep/ensembl-vep/envision.py /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700014_sorted.vcf > /exports/sascstudent/brenda/envision/envision_700014_sorted.vcf
 python /exports/sascstudent/brenda/vep/ensembl-vep/envision.py /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700102_sorted.vcf > /exports/sascstudent/brenda/envision/envision_700102_sorted.vcf
 python /exports/sascstudent/brenda/vep/ensembl-vep/envision.py /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700232_sorted.vcf > /exports/sascstudent/brenda/envision/envision_700232_sorted.vcf
@@ -5,6 +6,8 @@ python /exports/sascstudent/brenda/vep/ensembl-vep/envision.py /exports/sascstud
 python /exports/sascstudent/brenda/vep/ensembl-vep/envision.py /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700245_sorted.vcf > /exports/sascstudent/brenda/envision/envision_700245_sorted.vcf
 python /exports/sascstudent/brenda/vep/ensembl-vep/envision.py /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/D1_sorted.vcf > /exports/sascstudent/brenda/envision/envision_D1_sorted.vcf
 
+#For each family, a subset csv file consisting of the information found in the original envision csv file for the variants in the current VCF file will be made.
+#First, each file is created using the header from the original csv file.
 cat /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv | head -n 1 > /exports/sascstudent/brenda/envision/envision_700014.csv
 cat /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv | head -n 1 > /exports/sascstudent/brenda/envision/envision_700102.csv
 cat /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv | head -n 1 > /exports/sascstudent/brenda/envision/envision_700232.csv
@@ -12,6 +15,8 @@ cat /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv |
 cat /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv | head -n 1 > /exports/sascstudent/brenda/envision/envision_700245.csv
 cat /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv | head -n 1 > /exports/sascstudent/brenda/envision/envision_D1.csv
 
+#Next, each variant that is both in the exomiser result and the original csv file will be added to the subset csv.
+#The two files are compared using the Uniprot id, the protein position and the alternative protein.
 awk -F "[,/ ]" 'FNR==NR{a[$1,$2,$3]=$0;next}{if(b=a[$4,$5,$6]){print $0}}' /exports/sascstudent/brenda/envision/envision_700014_sorted.vcf /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv >> /exports/sascstudent/brenda/envision/envision_700014.csv
 awk -F "[,/ ]" 'FNR==NR{a[$1,$2,$3]=$0;next}{if(b=a[$4,$5,$6]){print $0}}' /exports/sascstudent/brenda/envision/envision_700102_sorted.vcf /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv >> /exports/sascstudent/brenda/envision/envision_700102.csv
 awk -F "[,/ ]" 'FNR==NR{a[$1,$2,$3]=$0;next}{if(b=a[$4,$5,$6]){print $0}}' /exports/sascstudent/brenda/envision/envision_700232_sorted.vcf /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv >> /exports/sascstudent/brenda/envision/envision_700232.csv
@@ -19,6 +24,7 @@ awk -F "[,/ ]" 'FNR==NR{a[$1,$2,$3]=$0;next}{if(b=a[$4,$5,$6]){print $0}}' /expo
 awk -F "[,/ ]" 'FNR==NR{a[$1,$2,$3]=$0;next}{if(b=a[$4,$5,$6]){print $0}}' /exports/sascstudent/brenda/envision/envision_700245_sorted.vcf /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv >> /exports/sascstudent/brenda/envision/envision_700245.csv
 awk -F "[,/ ]" 'FNR==NR{a[$1,$2,$3]=$0;next}{if(b=a[$4,$5,$6]){print $0}}' /exports/sascstudent/brenda/envision/envision_D1_sorted.vcf /exports/sascstudent/brenda/envision/human_predicted_combined_20170925.csv >> /exports/sascstudent/brenda/envision/envision_D1.csv
 
+#Next, each of the variants that has a score higher than 0.5 will be written to the envision output file for each family.
 python /exports/sascstudent/brenda/vep/ensembl-vep/envision_filters.py /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700014_sorted.vcf /exports/sascstudent/brenda/envision/envision_700014.csv /exports/sascstudent/brenda/envision/700014_envision_filtered.vcf
 python /exports/sascstudent/brenda/vep/ensembl-vep/envision_filters.py /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700014_sorted.vcf /exports/sascstudent/brenda/envision/envision_700102.csv /exports/sascstudent/brenda/envision/700102_envision_filtered.vcf
 python /exports/sascstudent/brenda/vep/ensembl-vep/envision_filters.py /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700232_sorted.vcf /exports/sascstudent/brenda/envision/envision_700232.csv /exports/sascstudent/brenda/envision/700232_envision_filtered.vcf
