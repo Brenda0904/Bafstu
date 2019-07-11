db9af1f897df7fbab6562e799e8441ef1c712c3e
Brenda0904 <brenda_vd_berg@live.nl>
2019-07-11 09:56:26 +0200

    Update Bafstu_script.sh

--- Bafstu_script.sh
+++ Bafstu_script.sh
@@ -10,6 +10,7 @@
 #$ -M s1095204@student.hsleiden.nl
 #$ -pe BWA 8
 
+#This script executes all of the tools in the order: VEP, exomiser, envision, fathmmxf.
 cd /exports/sascstudent/brenda/vep/ensembl-vep/
 ./vep -i /exports/sascstudent/brenda/multisample.final.vcf --offline --cache --dir_cache /exports/sascstudent/brenda/.vep/ --plugin CADD,/exports/sascstudent/brenda/vep/ensembl-vep/whole_genome_SNVs.tsv.gz,/exports/sascstudent/brenda/vep/ensembl-vep/InDels.tsv.gz --uniprot --protein --sift b --polyphen b --vcf --fork 8 --buffer_size 50 -o /exports/sascstudent/vep/ensembl-vep/vep_out.vcf
 python /exports/sascstudent/brenda/vep/ensembl-vep/vep_filter.py /exports/sascstudent/brenda/vep/ensembl-vep/vep_out.vcf /exports/sascstudent/brenda/vep/ensembl-vep/vep_filtered.vcf
