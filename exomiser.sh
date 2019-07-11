284f185e8711400f5270cb9e07d0fce4c40f1457
Brenda0904 <brenda_vd_berg@live.nl>
2019-07-11 10:02:55 +0200

    Update exomiser.sh

--- exomiser.sh
+++ exomiser.sh
@@ -1,15 +1,22 @@
+#The original multisample VCF file is split into the six different families. 
+#Samples_700014.txt, for example, contains a list of samples in family 700014.
 bcftools view -Ov -S /exports/sascstudent/brenda/samples_700014.txt /exports/sascstudent/brenda/vep_filtered.vcf > /exports/sascstudent/brenda/exomiser-cli-11.0.0/family_700014.vcf
 bcftools view -Ov -S /exports/sascstudent/brenda/samples_700102.txt /exports/sascstudent/brenda/vep_filtered.vcf > /exports/sascstudent/brenda/exomiser-cli-11.0.0/family_700102.vcf
 bcftools view -Ov -S /exports/sascstudent/brenda/samples_700232.txt /exports/sascstudent/brenda/vep_filtered.vcf > /exports/sascstudent/brenda/exomiser-cli-11.0.0/family_700232.vcf
 bcftools view -Ov -S /exports/sascstudent/brenda/samples_700239.txt /exports/sascstudent/brenda/vep_filtered.vcf > /exports/sascstudent/brenda/exomiser-cli-11.0.0/family_700239.vcf
 bcftools view -Ov -S /exports/sascstudent/brenda/samples_700245.txt /exports/sascstudent/brenda/vep_filtered.vcf > /exports/sascstudent/brenda/exomiser-cli-11.0.0/family_700245.vcf
 bcftools view -Ov -S /exports/sascstudent/brenda/samples_D1.txt /exports/sascstudent/brenda/vep_filtered.vcf > /exports/sascstudent/brenda/exomiser-cli-11.0.0/family_D1.vcf
+
+#Exomiser is run for each of the six VCF files that were just created by splitting the families.
 java -Xmx200g -jar /exports/sascstudent/brenda/exomiser-cli-11.0.0/exomiser-cli-11.0.0.jar --analysis /exports/sascstudent/brenda/exomiser-cli-11.0.0/analysis_file_700014.yml
 java -Xmx200g -jar /exports/sascstudent/brenda/exomiser-cli-11.0.0/exomiser-cli-11.0.0.jar --analysis /exports/sascstudent/brenda/exomiser-cli-11.0.0/analysis_file_700232.yml
 java -Xmx200g -jar /exports/sascstudent/brenda/exomiser-cli-11.0.0/exomiser-cli-11.0.0.jar --analysis /exports/sascstudent/brenda/exomiser-cli-11.0.0/analysis_file_700239.yml
 java -Xmx200g -jar /exports/sascstudent/brenda/exomiser-cli-11.0.0/exomiser-cli-11.0.0.jar --analysis /exports/sascstudent/brenda/exomiser-cli-11.0.0/analysis_file_700245.yml
 java -Xmx200g -jar /exports/sascstudent/brenda/exomiser-cli-11.0.0/exomiser-cli-11.0.0.jar --analysis /exports/sascstudent/brenda/exomiser-cli-11.0.0/analysis_file_D1.yml
 java -Xmx200g -jar /exports/sascstudent/brenda/exomiser-cli-11.0.0/exomiser-cli-11.0.0.jar --analysis /exports/sascstudent/brenda/exomiser-cli-11.0.0/analysis_file_700102.yml
+
+#Each of the output files from exomiser is sorted so it can be used to run the next tool. 
+#In order to sort the VCF files, they have to be compressed first.
 bgzip /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700014.vcf
 vcf-sort /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700014.vcf.gz > 700014_sorted.vcf
 bgzip /exports/sascstudent/brenda/exomiser-cli-11.0.0/results/700014_sorted.vcf
